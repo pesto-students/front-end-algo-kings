19141aae7b2dbe184262497307dcb439b1b0189c
omdubey <omdubey1021@gmail.com>
2023-09-15 13:04:07 +0530

    fix:Removed import statement for Pay.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import Messages from "./pages/messages/Messages";
 import Message from "./pages/message/Message";
 import MyGigs from "./pages/myGigs/MyGigs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Pay from "./pages/pay/Pay";
 import Success from "./pages/success/Success";
 
 function App() {
